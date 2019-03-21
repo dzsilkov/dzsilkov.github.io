bcad5d43e11f0d5e6258f179f7270729fa21d74a
DzSilkov <DzSilkov@gmail.com>
2019-03-21 21:25:12 +0300

    fix semantic all project

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var slides = document.querySelectorAll('.sb-main__slide');
+var slides = document.querySelectorAll('.main__slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,7000);
 
 function nextSlide(){
-  slides[currentSlide].className = 'sb-main__slide';
+  slides[currentSlide].className = 'main__slide';
   currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 'sb-main__slide showing';
+  slides[currentSlide].className = 'main__slide showing';
 }
